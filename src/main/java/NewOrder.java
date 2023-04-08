9dc2bd755c26ee6d1d02258653ee2502b3290645
andre <deolliveira@gmail.com>
2023-04-08 10:11:55 -0300

    part_2_with_class

--- src/main/java/NewOrder.java
+++ src/main/java/NewOrder.java
@@ -1,9 +1,6 @@
 
 
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.Producer;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.*;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Properties;
@@ -14,14 +11,18 @@ public class NewOrder {
         var producer = new KafkaProducer<String, String>(properties());
         var value = "3511,0713,5678990";
         var record = new ProducerRecord<>( "Andre_topic", value, value);
-        producer.send(record, (data, ex) -> {
+        Callback callback = (data, ex) -> {
             if (ex != null) {
-
                 ex.printStackTrace();
                 return;
             }
             System.out.println(" SUCESSO ENVIANDO " + data.topic() + " partition " + data.partition() + " offset " + data.offset() + " timestamp " + data.timestamp());
-        }).get();
+        };
+
+        var email = "Teste streaming EMAIL";
+        var emailRecord = new ProducerRecord<>("ECOMMERCE_EMAIL", email, email);
+        producer.send(record, callback).get();
+        producer.send(emailRecord, callback).get();
     }
     private static Properties properties(){
         var properties = new Properties();
